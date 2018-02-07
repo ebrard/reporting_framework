20253e4c810609eafe13a854b1bf11ef71534836
Emmanuel <brard.e@gmail.com>
2018-02-07 22:21:18 +0100

    created new test case for feature composite business key

---
+++ test/testcases/GenerateDeltaReport/WithCompositeBusinessKey/CreateOrModifyData.py
@@ -0,0 +1,78 @@
+import dataset
+from faker import Faker
+from dateutil.relativedelta import relativedelta
+import datetime
+import sys
+import pickle as pickle
+import random
+
+file_created = "/Users/manu/Documents/ReportingFramework/test/testcases/GenerateDeltaReport/created.pickle"
+file_status_path = "/Users/manu/Documents/ReportingFramework/test/testcases/GenerateDeltaReport/file_status.dat"
+
+nb_customers = int(sys.argv[2])
+
+db = dataset.connect('sqlite:////Users/manu/Documents/ReportingFramework/test/data.db')
+
+table = db['customer']
+
+fake = Faker()
+
+fake_data_set = {}
+
+if str(sys.argv[1]) == "create":
+
+    with open(file_created, 'wb') as f:
+        for i in range(0, nb_customers):
+            birth_date = fake.date_between(start_date="-60y", end_date="today")
+            age = relativedelta(datetime.datetime.now(), birth_date).years
+
+            faked_data = dict(id=i,
+                              first_name=fake.first_name(),
+                              last_name=fake.last_name(),
+                              age=age,
+                              birthdate=birth_date)
+
+            fake_data_set[str(i)] = faked_data
+            table.insert(faked_data)
+
+        pickle.dump(fake_data_set, f)
+
+    db.commit()
+
+
+if str(sys.argv[1]) == "modify":
+
+    file_status = open(file_status_path, 'w')
+
+    with open(file_created, 'rb') as f_created:
+        initial_data = pickle.load(f_created)
+
+        for i in range(0, nb_customers):
+            record_key = initial_data[str(i)]["first_name"]+initial_data[str(i)]["last_name"]
+
+            dice = random.randint(0, 100)
+
+            if dice < 20:
+                # delete
+                file_status.write(record_key + " D\n")
+
+                db.query('delete from customer where id = %s' % i)
+
+            if dice > 80:
+                # Update
+                birth_date = fake.date_between(start_date="-60y", end_date="today")
+                age = relativedelta(datetime.datetime.now(), birth_date).years
+
+                db.query('update customer set age = %i where id = %s' % (age, i))
+
+                file_status.write(record_key + " U\n")
+                print("Customer with id " + str(i))
+
+            if 20 <= dice <= 80:
+                file_status.write(record_key + " S\n")
+
+        db.commit()
+        file_status.close()
+
+    file_status.close()
+
---
+++ test/testcases/GenerateDeltaReport/WithCompositeBusinessKey/Test_GenerateDeltaReport.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+function exit_on_error {
+    if [ $? != 0 ]; then
+        echo "Error"
+        exit 1
+    fi
+}
+
+root_path="/Users/manu/Documents/ReportingFramework"
+test_path="${root_path}/test/testcases/GenerateDeltaReport/WithCompositeBusinessKey"
+db_path="/Users/manu/Documents/ReportingFramework/test"
+
+nb_records=10
+nb_cycle=1
+
+echo "Creating report in the backend database"
+sqlite3 ${root_path}/db/backend.db < ${test_path}/CreateReport.sql
+exit_on_error
+
+echo "Create data in the source database"
+sqlite3 ${db_path}/data.db "delete from customer ;"
+exit_on_error
+
+python3 ${test_path}/CreateOrModifyData.py create ${nb_records}
+exit_on_error
+
+for i in {1..${nb_cycle}}
+do
+
+	echo "Running Report in delta mode"
+	time python ${root_path}/ReportingFramework/reporting_framework.py execute --name test --mode delta
+	exit_on_error
+
+	mv /Users/manu/Documents/ReportingFramework/test/testcases/GenerateDeltaReport/report.csv \
+	/Users/manu/Documents/ReportingFramework/test/testcases/GenerateDeltaReport/report_1.csv
+
+    echo "Modify data in the source database"
+	python3 ${test_path}/CreateOrModifyData.py modify ${nb_records}
+	exit_on_error
+
+	echo "Running Report in delta mode"
+	time python ${root_path}/ReportingFramework/reporting_framework.py execute --name test --mode delta
+	exit_on_error
+
+	mv /Users/manu/Documents/ReportingFramework/test/testcases/GenerateDeltaReport/report.csv \
+	/Users/manu/Documents/ReportingFramework/test/testcases/GenerateDeltaReport/report_2.csv
+
+	echo "Checking report content after data change"
+	python CheckResults.py
+	exit_on_error
+
+sleep 5
+done
+
+rm -f ../created.pickle
+rm -f file_status.dat
+rm -f report*.csv
